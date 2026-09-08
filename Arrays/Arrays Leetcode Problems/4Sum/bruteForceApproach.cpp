06cfbba2fdab3f700afe2fcea710400eca68417c
Darsh Chawla <darshdchawla@gmail.com>
2026-09-08 23:42:01 +0530

    4 sum (brute force approach)

--- Arrays/Arrays Leetcode Problems/4Sum/bruteForceApproach.cpp
+++ Arrays/Arrays Leetcode Problems/4Sum/bruteForceApproach.cpp
@@ -1,59 +1,38 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <set>
 using namespace std;
 
 vector<vector<int>> fourSum(vector<int> &nums, int target)
 {
-    vector<vector<int>> answer;
+    int n = nums.size();
+    set<vector<int>> s;
 
-    sort(nums.begin(), nums.end());
-
-    for (int i = 0; i < nums.size(); i++)
+    for (int i = 0; i < n; i++)
     {
-        if (i > 0 && nums[i] == nums[i - 1])
-            continue;
-
-        for (int j = i + 1; j < nums.size(); j++)
+        for (int j = i + 1; j < n; j++)
         {
-
-            if (j > i + 1 && nums[j] == nums[j - 1])
-                continue;
-
-            int k = j + 1;
-            int l = nums.size() - 1;
-
-            while (k < l)
+            for (int k = j + 1; k < n; k++)
             {
-                long long sum = nums[i];
-                sum += nums[j];
-                sum += nums[k];
-                sum += nums[l];
-
-                if (sum < target)
+                for (int l = k + 1; l < n; l++)
                 {
-                    k++;
-                }
-                else if (sum > target)
-                {
-                    l--;
-                }
-                else
-                {
-                    answer.push_back({nums[i], nums[j], nums[k], nums[l]});
-                    k++;
-                    l--;
+                    long long sum = nums[i] + nums[j];
+                    sum += nums[k];
+                    sum += nums[l];
+                    if (sum == target)
+                    {
+                        vector<int> temp{nums[i], nums[j], nums[k], nums[l]};
+                        sort(temp.begin(), temp.end());
 
-                    while (k < l && nums[k] == nums[k - 1])
-                        k++;
-
-                    while (k < l && nums[l] == nums[l + 1])
-                        l--;
+                        s.insert(temp);
+                    }
                 }
             }
         }
     }
 
+    vector<vector<int>> answer(s.begin(), s.end());
     return answer;
 }
 
