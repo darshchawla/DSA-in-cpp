baa9b13989611f86f6d1b14d574b9e9c37c39eae
Darsh Chawla <darshdchawla@gmail.com>
2026-09-10 18:55:05 +0530

    merge overlapping sub - intervals (brute force approach)

---
+++ Arrays/Merge Overlapping Sub - Intervals/bruteForceApproach.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<vector<int>> mergeIntervals(vector<vector<int>> &nums, int row, int column)
+{
+    int n = nums.size();
+    sort(nums.begin(), nums.end());
+
+    vector<vector<int>> answer;
+
+    for (int i = 0; i < n; i++)
+    {
+        int start = nums[i][0];
+        int end = nums[i][1];
+        if (!answer.empty() && end <= answer.back()[1])
+        {
+            continue;
+        }
+        for (int j = 0; j < n; j++)
+        {
+            if (nums[j][0] <= end)
+            {
+                end = max(end, nums[j][1]);
+            }
+            else
+            {
+                break;
+            }
+        }
+        answer.push_back({start, end});
+    }
+
+    return answer;
+}
+
+int main()
+{
+    int r;
+    cout << "Enter number of row : ";
+    cin >> r;
+
+    cout << endl;
+
+    int c = 2;
+
+    vector<vector<int>> nums(r, vector<int>(c));
+
+    cout << "Enter the intervals : " << endl;
+
+    cout << endl;
+
+    for (int i = 0; i < r; i++)
+    {
+        cout << "Interval " << i + 1 << " : ";
+        for (int j = 0; j < c; j++)
+        {
+            cin >> nums[i][j];
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+
+    cout << "The intervals are : " << endl;
+
+    for (int i = 0; i < r; i++)
+    {
+        cout << "{ " << nums[i][0] << ", " << nums[i][1] << " }" << endl;
+    }
+
+    cout << endl;
+    cout << endl;
+
+    vector<vector<int>> answer = mergeIntervals(nums, r, c);
+
+    cout << "Merged intervals are :" << endl;
+
+    for (int i = 0; i < answer.size(); i++)
+    {
+        cout << "{ " << answer[i][0] << ", " << answer[i][1] << " }" << endl;
+    }
+
+    return 0;
+}
