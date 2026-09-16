6164436900b8979e81c6d75748ac69ed1b11f1a3
Darsh Chawla <darshdchawla@gmail.com>
2026-09-16 18:37:41 +0530

    traversal in linked list

---
+++ Linked List/traversalInLinkedList.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+public:
+    Node(int data1, Node *next1)
+    {
+        data = data1;
+        next = next1;
+    }
+
+public:
+    Node(int data1)
+    {
+        data = data1;
+        next = nullptr;
+    }
+};
+
+Node *arrayToLinkedList(vector<int> &arr)
+{
+    Node *head = new Node(arr[0]);
+    Node *mover = head;
+    for (int i = 1; i < arr.size(); i++)
+    {
+        Node *temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+
+    return head;
+}
+
+int main()
+{
+    vector<int> arr = {2, 5, 7, 17};
+
+    Node *head = arrayToLinkedList(arr);
+
+    Node *temp = head;
+
+    while (temp)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+
+    return 0;
+}
