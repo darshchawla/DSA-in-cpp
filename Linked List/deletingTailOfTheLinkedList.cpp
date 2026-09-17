acc43bfd9f9446c120f503e4e3087d2a0f1f57d3
Darsh Chawla <darshdchawla@gmail.com>
2026-09-17 11:54:41 +0530

    deleting tail of the linked list

---
+++ Linked List/deletingTailOfTheLinkedList.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+public:
+    Node(int data1, Node *next1)
+    {
+        data = data1;
+        next = next1;
+    }
+
+public:
+    Node(int data1)
+    {
+        data = data1;
+        next = nullptr;
+    }
+};
+
+Node *arrayToLinkedList(vector<int> &arr)
+{
+    Node *head = new Node(arr[0]);
+    Node *mover = head;
+    for (int i = 1; i < arr.size(); i++)
+    {
+        Node *temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+
+    return head;
+}
+
+Node *removeTail(Node *head)
+{
+    if (head == NULL || head->next == NULL)
+        return NULL;
+
+    Node *temp = head;
+    while (temp->next->next != NULL)
+    {
+        temp = temp->next;
+    }
+
+    delete temp->next;
+    temp->next = nullptr;
+
+    return head;
+}
+
+void print(Node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    vector<int> arr = {2, 5, 7, 17};
+
+    Node *head = arrayToLinkedList(arr);
+
+    head = removeTail(head);
+
+    print(head);
+
+    return 0;
+}
