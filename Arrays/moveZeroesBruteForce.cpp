8c596b742dfb1509608edfec400a71f024aa8cbd
Darsh Chawla <darshdchawla@gmail.com>
2026-09-20 18:42:23 +0530

    move zeroes at the end (brute force approach)

---
+++ Arrays/moveZeroesBruteForce.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void moveZeroes(vector<int> &nums)
+{
+    vector<int> temp;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (nums[i] != 0)
+        {
+            temp.push_back(nums[i]);
+        }
+    }
+
+    while (temp.size() < nums.size())
+    {
+        temp.push_back(0);
+    }
+
+    nums = temp;
+}
+
+int main()
+{
+    vector<int> nums = {1, 0, 2, 3, 0, 4, 0, 1};
+
+    moveZeroes(nums);
+
+    for (int x : nums)
+    {
+        cout << x << " ";
+    }
+}
