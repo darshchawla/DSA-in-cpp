b7d7e27d5d5a0c15088fdd63252491aa0731cf6c
Darsh Chawla <darshdchawla@gmail.com>
2026-09-22 18:52:01 +0530

    check whether a number is prime or not (optimal approach)

--- Maths for DSA/primeOrNotOptimal.cpp
+++ Maths for DSA/primeOrNotOptimal.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
+#include <vector>
+#include <cmath>
 using namespace std;
 
 bool isPrime(int n)
 {
     int count = 0;
 
-    for (int i = 1; i <= n; i++) // Time complexity -> O(n);
+    for (int i = 1; i <= sqrt(n); i++)
     {
         if (n % i == 0)
         {
             count++;
+            if (n / i != i)
+            {
+                count++;
+            }
+        }
+        if (count > 2)
+        {
+            break;
         }
     }
 
