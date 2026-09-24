0ffe8f9687d122612eb84a1be8c708439aecd1ac
Darsh Chawla <darshdchawla@gmail.com>
2026-09-24 09:28:07 +0530

    encapsulation

--- OOPS/4 Pillars of OOPS/encapsulation.cpp
+++ OOPS/4 Pillars of OOPS/encapsulation.cpp
@@ -1,24 +1,65 @@
 #include <iostream>
 using namespace std;
 
-class Student
+class Student // Encapsulation
 {
-
 private:
     string name;
-    int age;
     int height;
+    int age;
 
 public:
+    void setName(string n)
+    {
+        name = n;
+    }
+
+    string getName()
+    {
+        return name;
+    }
+
+    void setHeight(int h)
+    {
+        height = h;
+    }
+
+    int getHeight()
+    {
+        return height;
+    }
+
+    void setAge(int a)
+    {
+        age = a;
+    }
+
     int getAge()
     {
-        return this->age;
+        return age;
     }
+};
+
+class Account // Uses of encapsulation
+{
+private:
+    double balance;  // data hiding
+    string password; // data hiding
+public:
+    string accountId;
+    string username;
 };
 
 int main()
 {
     Student first;
-     
+    first.setName("Darsh");
+    first.setHeight(175);
+    first.setAge(17);
+
+    cout << "Name : " << first.getName() << endl;
+    cout << "Height : " << first.getHeight() << " cm" << endl;
+    cout << "Age : " << first.getAge() << endl;
+
     return 0;
 }
